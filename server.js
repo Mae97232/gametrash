3af8ccbbfce764b6876d4ac66bbaa2fb13365350
Yorick <maelyck97232@gmail.com>
2025-05-15 12:18:45 +0200

    modification

--- server.js
+++ server.js
@@ -164,6 +164,8 @@ app.post('/webhook-stripe', express.raw({ type: 'application/json' }), async (re
         <p><strong>Produits commandés :</strong></p>
         <ul>${produits}</ul>
       `;
+console.log("SMTP USER:", process.env.SMTP_USER);
+console.log("SMTP PASS:", process.env.SMTP_PASS ? '✅ ok' : '❌ missing');
 
       const transporter = nodemailer.createTransport({
         host: 'smtp-relay.brevo.com',
